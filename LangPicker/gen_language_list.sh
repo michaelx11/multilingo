72e54caa32e4714618d37158d2783f1badcc8f8d
Michael Xu <michaeljxu11@gmail.com>
2015-12-03 22:34:21 -0800

    oops html is a language

--- LangPicker/gen_language_list.sh
+++ LangPicker/gen_language_list.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat RosettaCodeLanguageComparisonTable2015.html | sed -nr 's/<th> <span id="(.+)".*/\1/p' | sed -nr 's/^([^"]+)">.*/\1/p' > list_languages.txt
+cat RosettaCodeLanguageComparisonTable2015.txt | sed -nr 's/<th> <span id="(.+)".*/\1/p' | sed -nr 's/^([^"]+)">.*/\1/p' > list_languages.txt
